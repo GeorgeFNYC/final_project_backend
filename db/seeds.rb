2f8395128b90dd0a0c78ca755e6d784a04f0648a
George Fernandez <fernandezg704@gmail.com>
2022-11-30 20:05:53 -0500

    User id typo fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Seeding Test Users'
+User.create(first_name:'George', last_name:'Fernandez', username:'georgef21', password:'1039497')
